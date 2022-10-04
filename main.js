0e375ed9f26b8c34982a19a02d7a998d00547e38
purin <k24555569@gmail.com>
2022-10-04 21:37:45 +0900

    3일차

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-var loginbutton = document.getElementById('login');
-loginbutton.addEventListener("onmousedown", loginhandle);
+var loginbutton = document.getElementById("login");
+var registerbutton = document.getElementById("register");
+loginbutton.addEventListener("onmousedown", loginHandle);
+registerbutton.addEventListener("onmousedown", moveToRegister)
 
-function loginhandle() {
+function loginHandle() {
+  var id = document.getElementById("id").value;
+  if(id === NULL) {
+    alert("ID를 입력하세요");
+    clearIDPW();
+    return;
+  }
+  var pw = document.getElementById("pw").value;
+  if(pw === NULL) {
+    alert("PW를 입력하세요");
+    clearIDPW();
+    return;
+  }
+  checkIDPW(id, pw);
+  clearIDPW();
+}
 
+function clearIDPW() {
+  var id = document.getElementById("id").value;
+  var pw = document.getElementById("pw").value;
+  id, pw = NULL, NULL;
+}
+
+function checkIDPW(id, pw) {
+  var xmlHttp = new XMLHttpRequest();
+  
+}
+
+function moveToRegister() {
+  location.ref = "register.html";
 }
